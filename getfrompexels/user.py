7680ca8f75463227154f6390eacdd2d95a5ce09f
R1DF <33981824+R1DF@users.noreply.github.com>
2023-06-15 08:08:46 +0600

    Made User dataclass

---
+++ getfrompexels/user.py
@@ -0,0 +1,26 @@
+# Imports
+from dataclasses import dataclass
+
+# User class
+@dataclass
+class User:
+    _name: str
+    _url: str
+    _pexels_id: int
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def url(self):
+        return self._url
+
+    @property
+    def pexels_id(self):
+        return self._pexels_id
+
+    @property
+    def username(self):
+        return "@" + self.url.split("@")[1]
+
