78fd4b738bb3de6d70ade7f819894dc10ed1c994
R1DF <33981824+R1DF@users.noreply.github.com>
2023-06-15 15:26:16 +0600

    Created PexelsVideoPicture class

--- getfrompexels/video_picture.py
+++ getfrompexels/video_picture.py
@@ -1,6 +1,18 @@
 # Imports
-
+from dataclasses import dataclass
 
 # Class
+@dataclass
 class PexelsVideoPicture:
-    pass
+    _pexels_id: int
+    _picture_url: str
+
+    # Properties
+    @property
+    def pexels_id(self) -> int:
+        return self._pexels_id
+
+    @property
+    def picture_url(self) -> str:
+        return self._picture_url
+
