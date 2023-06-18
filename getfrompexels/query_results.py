b5ac8380188ee2e6481bbebd132138c403c4b662
R1DF <33981824+R1DF@users.noreply.github.com>
2023-06-18 20:21:33 +0600

    Add universal QueryResults class to be returned when a search is made

---
+++ getfrompexels/query_results.py
@@ -0,0 +1,33 @@
+# Imports
+from dataclasses import dataclass
+
+# Class
+@dataclass
+class PexelsQueryResults:
+    _content: list
+    _url: str
+    _total_results: int
+    _page: int
+    _per_page: int
+
+    # Properties
+    @property
+    def content(self) -> list:
+        return self._content
+
+    @property
+    def url(self) -> str:
+        return self._url
+
+    @property
+    def total_results(self) -> int:
+        return self._total_results
+
+    @property
+    def page(self) -> int:
+        return self._page
+
+    @property
+    def per_page(self) -> int:
+        return self._per_page
+
