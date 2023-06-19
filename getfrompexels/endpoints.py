2452db626ab9de94457a4acc56ea522ec8672103
R1DF <33981824+R1DF@users.noreply.github.com>
2023-06-19 09:10:18 +0600

    Added new endpoint

--- getfrompexels/endpoints.py
+++ getfrompexels/endpoints.py
@@ -2,5 +2,6 @@ ENDPOINTS = {
     "FIND_PHOTO": "https://api.pexels.com/v1/photos",
     "FIND_VIDEO": "https://api.pexels.com/videos/videos",
     "CURATED_PHOTOS": "https://api.pexels.com/v1/curated",
-    "POPULAR_VIDEOS": "https://api.pexels.com/videos/popular"
+    "POPULAR_VIDEOS": "https://api.pexels.com/videos/popular",
+    "FEATURED_COLLECTIONS": "https://api.pexels.com/v1/collections/featured"
 }
