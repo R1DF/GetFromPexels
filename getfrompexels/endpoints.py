e5f9c1ab7b5a4f5428953adcc78987fa0ba468ae
R1DF <33981824+R1DF@users.noreply.github.com>
2023-06-20 10:57:23 +0600

    Added 2 more endpoints

--- getfrompexels/endpoints.py
+++ getfrompexels/endpoints.py
@@ -3,5 +3,7 @@ ENDPOINTS = {
     "FIND_VIDEO": "https://api.pexels.com/videos/videos",
     "CURATED_PHOTOS": "https://api.pexels.com/v1/curated",
     "POPULAR_VIDEOS": "https://api.pexels.com/videos/popular",
-    "FEATURED_COLLECTIONS": "https://api.pexels.com/v1/collections/featured"
+    "FEATURED_COLLECTIONS": "https://api.pexels.com/v1/collections/featured",
+    "USER_COLLECTIONS": "https://api.pexels.com/v1/collections",
+    "COLLECTION_MEDIA": "https://api.pexels.com/v1/collections" # Same as USER_COLLECTIONS but kept separate just in case
 }
